94f19c2c3f34860c675e1f1c5b3482326eafc297
Sushil Pratap Bharati <sushil.bharati007@gmail.com>
2016-06-07 12:49:49 -0500

    Update test_precision.m

--- test_precision.m
+++ test_precision.m
@@ -4,17 +4,17 @@
 % top left corner of the bounding box)
 clear;
 A=[]; B =[];
-fileID1 = fopen('output_aircraft.txt','r'); %output file
-fileID2 = fopen('aircraft_300grtruth.txt','r'); %ground truth file
+fileID1 = fopen('output_skater_KCFonly.txt','r'); %output file
+fileID2 = fopen('skater_160_truth.txt','r'); %ground truth file
 tline1 = fgetl(fileID1);
 tline2 = fgetl(fileID2);
 while (ischar(tline1))
     C1 = textscan (tline1,'%d,%d,%d,%d');
     C2 = textscan (tline2,'%d,%d,%d,%d');
-    M1 = (double(C1{3}) - double(C1{1}))/2.0;
-    N1 = (double(C1{4}) - double(C1{2}))/2.0;
-    M2 = (double(C2{3}) - double(C2{1}))/2.0;
-    N2 = (double(C2{4}) - double(C2{2}))/2.0;
+    M1 = double (C1{1}) + ((double(C1{3}) - double(C1{1}))/2.0);
+    N1 = double (C1{2}) + ((double(C1{4}) - double(C1{2}))/2.0);
+    M2 = double (C2{1}) + ((double(C2{3}) - double(C2{1}))/2.0);
+    N2 = double (C2{2}) + ((double(C2{4}) - double(C2{2}))/2.0);
     Z1 = [M1 N1];
     Z2 = [M2 N2];
     A = vertcat (A,Z1);  
